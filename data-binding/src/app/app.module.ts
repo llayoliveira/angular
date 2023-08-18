8f3ec1035509bf8291233a67b9855ee75b001342
llayoliveira <layane2110@hotmail.com>
2023-08-18 10:52:44 -0300

    Ciclo de vida do componente

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { MeuFormModule } from './meu-form/meu-form.module';
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
-import { CicleComponent } from './cicle/cicle.component';
 import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
@@ -16,7 +15,6 @@ import { CicloComponent } from './ciclo/ciclo.component';
     DataBindingComponent,
     InputPropertyComponent,
     OutputPropertyComponent,
-    CicleComponent,
     CicloComponent
   ],
   imports: [
---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,60 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Input
+} from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @Input() valorInicial: number = 10;
+
+  constructor() {
+    console.log('constructor');
+   }
+
+   ngOnChanges() {
+    console.log('ngOnChanges');
+   }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+
+
+}
