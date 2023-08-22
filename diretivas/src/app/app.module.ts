ba40566b7689f152d972b97215021249a819a2c4
llayoliveira <layane2110@hotmail.com>
2023-08-22 11:19:36 -0300

    Diretiva ngSwitch, ngSwitchCase, ngSwitchDefault

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    TesteComponent
+    TesteComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
