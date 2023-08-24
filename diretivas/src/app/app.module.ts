2a179f2a635132b96a4e1c4c533fdfa74447c4e1
llayoliveira <layane2110@hotmail.com>
2023-08-24 13:45:54 -0300

    Diretiva de estrutura ngElse

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCutomizadasComponent } from './diretivas-cutomizadas/diretivas-cutomizadas.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 import { HighlightDirective } from './shared/highlight.directive';
+import { NgElseDirective } from './shared/ng-else.directive';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { HighlightDirective } from './shared/highlight.directive';
     FundoAmareloDirective,
     DiretivasCutomizadasComponent,
     HighlightMouseDirective,
-    HighlightDirective
+    HighlightDirective,
+    NgElseDirective
   ],
   imports: [
     BrowserModule,
