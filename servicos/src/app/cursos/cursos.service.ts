59dcbbff72a0b625b5e8b98ee3d0ad59d5e6b088
llayoliveira <layane2110@hotmail.com>
2023-08-30 13:54:52 -0300

    Instâncias de serviços e módulos

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CursosService {
-    getCursos(){
-        return ['Angular', 'Java', 'Python'];
+
+    private cursos: string[] = ['Angular', 'Java', 'Python'];
+
+    constructor() {
+        console.log('CursosService');
+    }
+
+    getCursos() {
+        return this.cursos;
+    }
+
+    addCurso(curso: string) {
+        this.cursos.push(curso);
     }
 }
