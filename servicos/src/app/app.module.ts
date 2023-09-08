65bd5354416e37fe70fe1f95fabd5baa8dd3324a
llayoliveira <layane2110@hotmail.com>
2023-09-08 17:59:53 -0300

    Comunicação entre componentes utilizando Serviços

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
 
